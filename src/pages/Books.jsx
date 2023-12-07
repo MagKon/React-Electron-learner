7b9aca406ecca9df167d26b2231441741d04c99f
MagKon <magnus.kongstad@gmail.com>
2023-12-07 09:29:59 +0100

    Added stuff

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -10,40 +10,42 @@ function Books() {
 
   return (
     <>
-      <h1>Books</h1>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Info</th>
-          </tr>
-        </thead>
-        <tbody>
-          {books.map((book) => {
-            return (
-              <tr key={book.id}>
-                <td>
-                  <NavLink
-                    to={"" + book.id}
-                    relative="path"
-                    className={({ isActive, isPending }) =>
-                      isPending
-                        ? "pending nav-link"
-                        : isActive
-                        ? "active nav-link"
-                        : "nav-link"
-                    }
-                  >
-                    {book.title}
-                  </NavLink>
-                </td>
-                <td>{book.info}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <Outlet context={[books, setBooks]} />
+      <div className="row">
+        <h1>Books</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Info</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book) => {
+              return (
+                <tr key={book.id}>
+                  <td>
+                    <NavLink
+                      to={"" + book.id}
+                      relative="path"
+                      className={({ isActive, isPending }) =>
+                        isPending
+                          ? "pending nav-link"
+                          : isActive
+                          ? "active nav-link"
+                          : "nav-link"
+                      }
+                    >
+                      {book.title}
+                    </NavLink>
+                  </td>
+                  <td>{book.info}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <Outlet context={[books, setBooks]} />
+      </div>
     </>
   );
 }
